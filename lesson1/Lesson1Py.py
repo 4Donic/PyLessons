838ff9cc1f9ae4222715545b6375878e91f6884c
Dima Ok <4Donic@gmail.com>
2019-09-29 02:01:02 +0300

    1st hw

--- lesson1/Lesson1Py.py
+++ lesson1/Lesson1Py.py
@@ -2,7 +2,7 @@ x = int(input("Введите Х "))
 
 y = int(input("Введите У "))
 print(x, y)
-sum = x + y
-print("Сумма этих чисел =", sum)
+s = x + y
+print("Сумма этих чисел =", s)
 step = x ** y
 print("Х в степени У =", step)
