3802e834d62b19ea5a1a49e7d768fdd71915b269
4Donic <4Donic@gmail.com>
2019-09-29 12:02:50 +0300

    Add files via upload

--- lesson1/Lesson1Py.py
+++ lesson1/Lesson1Py.py
@@ -1,8 +1,21 @@
-x = int(input("Введите Х "))
-y = int(input("Введите У "))
+
+
+print(' ****   *   *  *   *   *  \n'
+      ' *  *   *  **  ** **  * * \n'
+      ' *  *   * * *  * * *  *** \n'
+      '******  **  *  *   *  * * \n'
+      '*    *  *   *  *   *  * *')
+
+
+
+x = float(input('Введите X \n'))
+y = float(input('Введите У \n'))
 
 print(x, y)
-s = x + y
-print("Сумма этих чисел =", s)
+summ = x + y
+print("Сумма этих чисел =", summ)
 step = x ** y
 print("Х в степени У =", step)
+div = x // y
+ost = x % y  #(div * y - x)*-1
+print("Х, целоделенный на У =", div, '\n' "Остаток =", ost)
