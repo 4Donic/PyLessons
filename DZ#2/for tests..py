d120ca472d7852fb5579b09625ed3e8af4abe770
Dima Ok <4Donic@gmail.com>
2019-09-29 23:24:55 +0300

    Второе ДЗ, закончил второе задание

--- DZ#2/for tests..py
+++ DZ#2/for tests..py
@@ -1,5 +1,11 @@
 n = 12345
+print('n=',n)
 nr = int(str(n)[::-1])
-print(nr)
+print('nr=',nr)
 nr2 = nr + 4
-print(nr2)
+print('nr2=',nr2)
+
+num = int(input("Введите нужное натуральное число: "))
+print('num=',num)
+num += 1
+print('num=',num)
