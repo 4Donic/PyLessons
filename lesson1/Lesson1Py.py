4e54385d40db87dc0bf9f6985c44fc0e2d0dfde5
Dima Ok <4Donic@gmail.com>
2019-09-29 14:37:02 +0300

    Поменял местами степень

--- lesson1/Lesson1Py.py
+++ lesson1/Lesson1Py.py
@@ -13,8 +13,8 @@ y = float(input('Введите У \n'))
 print(x, y)
 summ = x + y
 print("Сумма этих чисел =", summ)
+div = x // y
+ost = x % y  # (div * y - x)*-1
+print("Х, целоделенный на У =", div, '\n' "Остаток =", ost)
 step = x ** y
 print("Х в степени У =", step)
-div = x // y
-ost = x % y  #(div * y - x)*-1
-print("Х, целоделенный на У =", div, '\n' "Остаток =", ost)
