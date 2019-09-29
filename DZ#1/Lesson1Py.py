2d216f8812ad2eac27c0b9a877ce717908c5a5fe
Dima Ok <4Donic@gmail.com>
2019-09-29 22:52:22 +0300

    Revert "Конечные штрихи№3"
    This reverts commit 84c0e63cd37eb1cb46a6161b955859f76e4b5bee.

--- DZ#1/Lesson1Py.py
+++ DZ#1/Lesson1Py.py
@@ -29,5 +29,5 @@ print("Сумма этих чисел =", summ)
 div = x // y
 ost = x % y  # (div * y - x)*-1
 print("Х, целоделенный на У =", div, '\n' "Остаток от деления =", ost)
-step = y ** x
+step = x ** y
 print("Х в степени У =", step)
