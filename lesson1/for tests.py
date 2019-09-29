a7055e7fc69753c3484ae3092838c96a68cdce1e
Dima Ok <4Donic@gmail.com>
2019-09-29 15:29:21 +0300

    Смена назвы
    Переименовал папку первого задания

--- lesson1/for tests.py
+++
@@ -1,8 +0,0 @@
-print('Escape sequences\n'
-      '\\a      Bell (alert)\n'
-      '\\b      Backspace\n'
-      '\\n      New line\n'
-      '\\t      Horizontal tab\n'
-      '\\\      Backslash \\ \n'
-      '\\”      Double quotation mark “\n'
-      '\\’      Single quotation mark ‘')
