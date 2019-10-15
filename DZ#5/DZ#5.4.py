37b5d8728120df3564e5bd39487a9c22f8badb0f
Dima Ok <4Donic@gmail.com>
2019-10-16 00:23:24 +0300

    сделал 5.4 ( По сути добавил лишь одно значение)

--- DZ#5/DZ#5.4.py
+++ DZ#5/DZ#5.4.py
@@ -1,18 +1,18 @@
 """
 При помощи вложенных циклов (можно while, можно for) нарисовать следующие фигуры
 представленные ниже. Пользователь вводит, с клавиатуры, высоту фигуры в символах.
-  C         *
+  D         *
           * * *
         * * * * *
       * * * * * * *
     * * * * * * * * *
   * * * * * * * * * * *
 * * * * * * * * * * * * *
-  *                   *
-    *               *
-      *           *
-        *       *
-          *   *
+  *         *         *
+    *       *       *
+      *     *     *
+        *   *   *
+          * * *
             *
 
 """
@@ -22,8 +22,8 @@ horizontal = hor = ver
 for i in range(ver):
     print(i+1, end='\t')
     for j in range(hor):
-        if j == hor//2+i or j == hor//2-i or hor//2-i < j < hor//2+i and i <= ver//2 or j == -hor//2+i+1 or j == hor-1-i+hor//2:
-
+        if j == hor//2+i or j == hor//2-i or hor//2-i < j < hor//2+i and i <= ver//2 or j == -hor//2+i+1 or j == hor-1-i+hor//2 or j == hor//2:
+                                                                                                                        # Вся суть 5.4 ^^^^^^
             print('* ', end='')
         else:
             print('  ', end='')
