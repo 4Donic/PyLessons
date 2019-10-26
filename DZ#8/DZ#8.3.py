5b8c665140b108799ad271c89c1ac62e0320670f
4Donic <4Donic@gmail.com>
2019-10-27 01:53:01 +0300

    8.3, 8.4

---
+++ DZ#8/DZ#8.3.py
@@ -0,0 +1,23 @@
+"""
+4. Написать функцию `season`, принимающую 1 аргумент — номер месяца (от 1 до 12), и возвращающую время года, которому
+этот месяц принадлежит (зима, весна, лето или осень).
+"""
+
+
+def season(month):
+    if 1 <= month <= 12:
+        if month == 12 or 1 <= month <= 2:
+            print("Zima")
+        elif 3 <= month <= 5:
+            print("Vesna")
+        elif 6 <= month <= 9:
+            print("Leto")
+        else:
+            print("Osen")
+    else:
+        print("Вы ввели несуществующий номер месяца\n")
+
+
+month = int(input("Введине номер месяца"))
+
+season(month)
