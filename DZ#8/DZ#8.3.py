08b9b44847a202fa3b242e9b65aa14488fba16bf
4Donic <4Donic@gmail.com>
2019-10-27 10:05:18 +0200

    Update DZ#8.3.py

--- DZ#8/DZ#8.3.py
+++ DZ#8/DZ#8.3.py
@@ -8,12 +8,16 @@ def season(month):
     if 1 <= month <= 12:
         if month == 12 or 1 <= month <= 2:
             print("Zima")
+            return "Zima"
         elif 3 <= month <= 5:
             print("Vesna")
+            return "Vesna"
         elif 6 <= month <= 9:
             print("Leto")
+            return "Leto"
         else:
             print("Osen")
+            return "Osen"
     else:
         print("Вы ввели несуществующий номер месяца\n")
 
